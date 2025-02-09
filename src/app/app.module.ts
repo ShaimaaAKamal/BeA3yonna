ec153668dee337d7c3772591ca85407e48ee60d0
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-02-09 16:23:47 +0200

    add directive to use select2 within the project  and add http provider and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './Components/shared/navbar/navbar.component';
@@ -8,6 +7,10 @@ import { FooterComponent } from './Components/shared/footer/footer.component';
 import { FlagCardComponent } from './Components/Main/flag-card/flag-card.component';
 import { SymptomCardComponent } from './Components/Main/symptom-card/symptom-card.component';
 import { HomeComponent } from './Components/Main/home/home.component';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { Select2Directive } from './Dirctives/select2.directive';
+import { ButtonWithIconComponent } from './Components/shared/button-with-icon/button-with-icon.component';
+import { BackButtonComponent } from './Components/shared/back-button/back-button.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +19,18 @@ import { HomeComponent } from './Components/Main/home/home.component';
     FooterComponent,
     FlagCardComponent,
     SymptomCardComponent,
-    HomeComponent
+    HomeComponent,
+    ButtonWithIconComponent,
+    BackButtonComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Select2Directive
   ],
-  providers: [],
+  providers: [
+      provideHttpClient(withInterceptorsFromDi())
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
