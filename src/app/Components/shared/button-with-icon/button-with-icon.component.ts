35410ac9ce7e8f5d47bb11c669fb1adbf50def19
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-02-09 16:05:15 +0200

    finish button with icon componenet implementation

---
+++ src/app/Components/shared/button-with-icon/button-with-icon.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button-with-icon',
+  standalone: false,
+  
+  templateUrl: './button-with-icon.component.html',
+  styleUrl: './button-with-icon.component.css'
+})
+export class ButtonWithIconComponent {
+@Input() btnAction:string='';
+@Input() disabled:boolean=false;
+}
