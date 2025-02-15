912ee970d705826c5c82391bb91a43fe6ba11439
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-02-15 12:46:21 +0200

    update select2 directive to presist the last language selection

--- src/app/Dirctives/select2.directive.ts
+++ src/app/Dirctives/select2.directive.ts
@@ -8,16 +8,28 @@ declare var window: any; // Declare window as a global object
 })
 export class Select2Directive implements AfterViewInit, OnDestroy {
   @Output() valueChange = new EventEmitter<string>(); // Emits when value changes
+
   constructor(private el: ElementRef) {}
 
+
   ngAfterViewInit(): void {
-    window.$(this.el.nativeElement).select2(); 
-     // Handle change event
+    this.initializeSelect2();
+
+    setTimeout(() => {
+      const savedValue = localStorage.getItem('lang');
+      if (savedValue)
+        window.$(this.el.nativeElement).val(savedValue).trigger('change');
+    }, 130);
      window.$(this.el.nativeElement).on('change', (event:any) => {
       this.valueChange.emit(event.target.value);
     });
   }
 
+   initializeSelect2(): void {
+    setTimeout(() => {
+      window.$(this.el.nativeElement).select2();
+    }, 100); // Delay to ensure translation is applied
+  }
   ngOnDestroy(): void {
     window.$(this.el.nativeElement).select2('destroy');
   }
