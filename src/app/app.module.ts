b715cb759d4c52ff9e4d3b5649a53405fbc13ec1
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-02-15 12:50:37 +0200

    add tranlsation package and set ngx tranlsation loader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,17 @@ import { FooterComponent } from './Components/shared/footer/footer.component';
 import { FlagCardComponent } from './Components/Main/flag-card/flag-card.component';
 import { SymptomCardComponent } from './Components/Main/symptom-card/symptom-card.component';
 import { HomeComponent } from './Components/Main/home/home.component';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { Select2Directive } from './Dirctives/select2.directive';
 import { ButtonWithIconComponent } from './Components/shared/button-with-icon/button-with-icon.component';
 import { BackButtonComponent } from './Components/shared/back-button/back-button.component';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { StyleService } from './Services/style/style.service';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -26,11 +33,27 @@ import { BackButtonComponent } from './Components/shared/back-button/back-button
   imports: [
     BrowserModule,
     AppRoutingModule,
-    Select2Directive
+    Select2Directive,
+      TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      },
+      defaultLanguage: 'en', // Set default language
+    })
   ],
   providers: [
       provideHttpClient(withInterceptorsFromDi())
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+    constructor(private __TranslateService: TranslateService,private __StyleService:StyleService) {
+    this.__TranslateService.setDefaultLang('en'); // Default to English
+    const savedLang = localStorage.getItem('lang') || 'en';
+    this.__TranslateService.use(savedLang);
+    const isRTL:boolean=(savedLang == 'ar')?true:false;
+    this.__StyleService.switchStyleToRTL(isRTL,savedLang);
+  }
+}
