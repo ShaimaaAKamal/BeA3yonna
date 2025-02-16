3ceb07bebfd81eeebaf92305847f01ca5d57329c
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-02-16 13:24:08 +0200

    finish back button implementation

---
+++ src/app/Components/shared/back-button/back-button.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-back-button',
+  standalone: false,
+  
+  templateUrl: './back-button.component.html',
+  styleUrl: './back-button.component.css'
+})
+export class BackButtonComponent {
+@Input() disabled:boolean=true;
+constructor(){}
+}
