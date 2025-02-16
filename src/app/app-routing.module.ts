0151a94da6ef0eaf2a293ecc53326741e8d3225d
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-02-16 13:26:37 +0200

    add new route to record patient Info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Components/Main/home/home.component';
-import { ChooseFlagComponent } from './Components/Main/flag-card/choose-flag/choose-flag.component';
+import { ChooseFlagComponent } from './Components/Main/choose-flag/choose-flag.component';
+import { PatientInfoComponent } from './Components/Main/PatientInfo/patient-info/patient-info.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent ,pathMatch:'full'},
-    { path: 'Choose_Flag', component: ChooseFlagComponent}
+    { path: 'Choose_Flag', component: ChooseFlagComponent},
+    { path: 'Patient_Info', component: PatientInfoComponent}
+
 ];
 
 @NgModule({
