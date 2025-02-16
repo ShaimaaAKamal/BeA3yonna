0440e7f59f9a686405dcff7a35a21b6f3bb3a3c6
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-02-16 13:23:39 +0200

    init patient Info Component

---
+++ src/app/Components/Main/PatientInfo/patient-info/patient-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-info',
+  standalone: false,
+  
+  templateUrl: './patient-info.component.html',
+  styleUrl: './patient-info.component.css'
+})
+export class PatientInfoComponent {
+
+}
