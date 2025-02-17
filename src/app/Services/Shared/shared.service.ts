271af93df8e9f413299eb73870b72d6939c966de
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-02-17 13:44:14 +0200

    add more methods to shared service to used accross different component

--- src/app/Services/Shared/shared.service.ts
+++ src/app/Services/Shared/shared.service.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
 
-  constructor(private __HttpClient:HttpClient) { }
+  constructor(private __HttpClient:HttpClient,private __Router:Router) { }
 
   sendGetRequest(url:string):Observable<any>{
     return this.__HttpClient.get(url);
   }
   saveItemInLocalStorage(key:string,value:string){
     localStorage.setItem(key,value);
+  }
+  getItemFromLocalStorage(key:string):string{
+    return localStorage.getItem(key) ?? '';
+  }
+  navigateToPage(pageUrl:string){
+    this.__Router.navigate([pageUrl])
   }
 }
