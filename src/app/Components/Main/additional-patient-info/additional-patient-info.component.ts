e9c42cee9ba83e452e426ab5330f5fa4efe7b136
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-02-17 20:58:04 +0200

    finish additional patient info component

--- src/app/Components/Main/additional-patient-info/additional-patient-info.component.ts
+++ src/app/Components/Main/additional-patient-info/additional-patient-info.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from '../../../Services/Shared/shared.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { PatientAdditionalInfo } from '../../../Interfaces/patient-additional-info';
 
 @Component({
   selector: 'app-additional-patient-info',
@@ -7,6 +10,23 @@ import { Component } from '@angular/core';
   templateUrl: './additional-patient-info.component.html',
   styleUrl: './additional-patient-info.component.css'
 })
-export class AdditionalPatientInfoComponent {
-
+export class AdditionalPatientInfoComponent implements OnInit{
+additionalPatientInfo!:FormGroup;
+storedPatientAdditionalInfo:PatientAdditionalInfo|null=null;
+constructor(private __sharedService:SharedService){}
+ngOnInit(): void {
+this.storedPatientAdditionalInfo=this.__sharedService.getItemFromLocalStorage('additionalPatientInfo')?JSON.parse(this.__sharedService.getItemFromLocalStorage('additionalPatientInfo')):null;
+ this.additionalPatientInfo= new FormGroup({
+     pregnant: new FormControl( this.storedPatientAdditionalInfo?.pregnant ?? '',[Validators.required]),
+     havePeriod: new FormControl(this.storedPatientAdditionalInfo?.havePeriod ?? '', [Validators.required]),
+     additionalInfo:new FormControl( this.storedPatientAdditionalInfo?.pregnant ??''),
+   });
+}
+BackToPreviousPage(){
+this.__sharedService.navigateToPage('/Patient_Info');
+  }
+navigateNextPage(){
+this.__sharedService.saveItemInLocalStorage('additionalPatientInfo',JSON.stringify(this.additionalPatientInfo.value));
+this.__sharedService.navigateToPage('/Patient_Initial_Vitals');
+}
 }
