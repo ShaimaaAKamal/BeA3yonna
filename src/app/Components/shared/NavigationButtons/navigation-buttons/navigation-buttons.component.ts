f7cbe4846f442c45abe8e74f28a3a48dea69ae93
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-02-18 11:33:39 +0200

    implement navigation buttons component to be used in all cmponenets

---
+++ src/app/Components/shared/NavigationButtons/navigation-buttons/navigation-buttons.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+import { SharedService } from '../../../../Services/Shared/shared.service';
+
+@Component({
+  selector: 'app-navigation-buttons',
+  standalone: false,
+  
+  templateUrl: './navigation-buttons.component.html',
+  styleUrl: './navigation-buttons.component.css'
+})
+export class NavigationButtonsComponent {
+@Input() PreviousPageUrl:string='';
+@Input() NextPageUrl:string='';
+@Input() NextButtonDisabled:boolean=false;
+@Input() MoreCLasses:string='';
+@Input() Key:string='';
+@Input() value:any;
+constructor(private __SharedService:SharedService){}
+BackToPreviousPage(){
+ this.__SharedService.navigateToPage(this.PreviousPageUrl);
+}
+navigateNextPage()
+{ if(this.Key)
+    this.__SharedService.saveItemInLocalStorage(this.Key,JSON.stringify(this.value));
+ this.__SharedService.navigateToPage(this.NextPageUrl);
+}
+}
