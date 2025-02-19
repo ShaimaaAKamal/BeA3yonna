57234ad6971c3832465fadd5c8048c6fddba819b
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-02-20 00:41:41 +0200

    finish point pain areas

--- src/app/Components/Main/PainedBodyPart/pained-body-part/pained-body-part.component.ts
+++ src/app/Components/Main/PainedBodyPart/pained-body-part/pained-body-part.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {  Component, OnInit} from '@angular/core';
+import { SharedService } from '../../../../Services/Shared/shared.service';
 
 @Component({
   selector: 'app-pained-body-part',
@@ -7,6 +8,42 @@ import { Component } from '@angular/core';
   templateUrl: './pained-body-part.component.html',
   styleUrl: './pained-body-part.component.css'
 })
-export class PainedBodyPartComponent {
+export class PainedBodyPartComponent implements OnInit{
+NextButtonDisabled:boolean=true;
+selectedPartsIndexes:number[]=[];
+storedPainedParts:number[]=[];
 
+constructor(private __SharedService:SharedService){}
+
+ngOnInit(): void {
+const paths=document.getElementsByTagName('path');
+const pathsArray=Array.from(paths);
+this.storedPainedParts=this.__SharedService.getItemFromLocalStorage('painedParts')?JSON.parse(this.__SharedService.getItemFromLocalStorage('painedParts')):[];
+if(this.storedPainedParts) this.displayStoryedPainedParts(pathsArray);
+pathsArray.forEach((path,index) => {
+  path.addEventListener('click',()=>{
+      const existedIndex = this.selectedPartsIndexes.indexOf(index);
+      if(existedIndex != -1){
+          path.setAttribute('fill','none');
+          this.selectedPartsIndexes.splice(existedIndex,1);
+      }
+      else{
+      path.setAttribute('fill','red');
+      this.selectedPartsIndexes.push(index);
+    }
+    this.NextButtonDisabled=(this.selectedPartsIndexes.length !=0 )?false:true;
+  })
+});
+}
+
+displayStoryedPainedParts(pathsArray:any){
+  pathsArray.forEach((path:any,index:number) => {
+      const existedIndex = this.storedPainedParts.indexOf(index);
+      if(existedIndex != -1){
+          path.setAttribute('fill','red');
+        this.selectedPartsIndexes.push(index);
+      }
+});
+this.NextButtonDisabled=(this.selectedPartsIndexes.length !=0 )?false:true;
+}
 }
