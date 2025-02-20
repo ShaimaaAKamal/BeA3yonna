2163a2ca9f33442a789a4f4532a94c4d85358e00
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-02-20 10:42:43 +0200

    implement initial assessment component

---
+++ src/app/Components/Main/Assessment/assesment/assesment.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assesment',
+  standalone: false,
+  
+  templateUrl: './assesment.component.html',
+  styleUrl: './assesment.component.css'
+})
+export class AssesmentComponent {
+
+}
