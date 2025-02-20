97ea02373258b2f0d822cc618ef706ad96379c7b
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-02-20 12:55:44 +0200

    update patient history component to use generice method of getting data

--- src/app/Components/Main/PatientComplainDetails/patient-complain-details/patient-complain-details.component.ts
+++ src/app/Components/Main/PatientComplainDetails/patient-complain-details/patient-complain-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { SharedService } from '../../../../Services/Shared/shared.service';
+import { PatientHistory } from '../../../../Interfaces/patient-history';
 
 @Component({
   selector: 'app-patient-complain-details',
@@ -11,13 +12,13 @@ import { SharedService } from '../../../../Services/Shared/shared.service';
 })
 export class PatientComplainDetailsComponent implements OnInit {
   PatientHistory!:FormGroup;
-  stortedPatientHistory:any;
+  stortedPatientHistory!:PatientHistory;
   PreviousPageUrl!:string;
   constructor(private __SharedService:SharedService){}
 
   ngOnInit(): void {
     this.getPreviousPage();
-    this.stortedPatientHistory=this.__SharedService.getStoredDataValue('patientHistory');
+    this.stortedPatientHistory=this.__SharedService.getGenericStoredDataValue('patientHistory');
     this.PatientHistory= new FormGroup({
          complainTime: new FormControl( this.stortedPatientHistory?.complainTime ?? '',[Validators.required]),
          lastMealTime: new FormControl(this.stortedPatientHistory?.lastMealTime ?? '', [Validators.required]),
@@ -25,9 +26,8 @@ export class PatientComplainDetailsComponent implements OnInit {
   }
 
   getPreviousPage(){
-    console.log('in');
-  const PatientInitialVitals=this.__SharedService.getStoredDataValue('patientInitialVitals');
-  this.PreviousPageUrl=(PatientInitialVitals.length == 0  || PatientInitialVitals.havePeramentDiseases === 'no')
+  const PatientInitialVitals=this.__SharedService.getGenericStoredDataValue('patientInitialVitals');
+  this.PreviousPageUrl=(PatientInitialVitals.havePeramentDiseases === 'no')
                               ?'Choose_Pained_Body_Part':'Permanent Diseases';
  
   }
