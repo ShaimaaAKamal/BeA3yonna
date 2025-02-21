435903536e109d3bd762c3b82c1e175acf8489d5
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-02-21 18:04:04 +0200

    omplement patient report interface

---
+++ src/app/Interfaces/patient-report-data.ts
@@ -0,0 +1,26 @@
+import { AssessmentInfo } from "./assessment-info"
+import { Country } from "./country"
+import { PainScale } from "./pain-scale"
+import { PatientAdditionalInfo } from "./patient-additional-info"
+import { PatientHistory } from "./patient-history"
+import { PatientInfo } from "./patient-info"
+import { PatientInitialVitals } from "./patient-initial-vitals"
+import { PatientVitals } from "./patient-vitals"
+
+export interface PatientReportData {
+
+AssessmentInfo:AssessmentInfo,
+Country:Country,
+CountrycurrentPage:number,
+PermanentDiseases:string[]
+Symptoms:string[],
+additionalPatientInfo:PatientAdditionalInfo ,
+lang:string, 
+language: string,
+painScale:PainScale
+painedParts:number[],
+patientHistory:PatientHistory
+patientInfo:PatientInfo,
+patientInitialVitals:PatientInitialVitals,
+patientVitals:PatientVitals,
+}
